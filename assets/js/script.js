57ff87f8c2f0d493f2ba1726b8d0de6dbbaac347
Amir <amir.hackett@gmail.com>
2022-03-20 22:20:25 -0400

    added function for save btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
 // Display today's day and date
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
+
+// value for save button
+$(".saveBtn").on("click", function(){
+    var saveSched = $(this).siblings(".description").val()
+    var saveHour = $(this).parent().attr("id")
+    localStorage.setItem(saveHour, saveSched)
+})
+
+
+localStorage.getItem("hour8")
